507cc436ac4c099cf3dfcaed3eb6cf58d3f3e753
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-11 11:33:27 +0100

    here we use a special attribute that will let us display certain fields for view in the admin area.

--- gamers_blog/admin.py
+++ gamers_blog/admin.py
@@ -4,6 +4,14 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import BlogPost
 
-
 # Register your models here.
+
+
+# Customise the admin area so we can display more fields
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('post_title', 'post_slug', 'post_author', 'publish',
+                    'post_status')
+
+
+# Register our BlogPost & BlogPostAdmin to the admin area.
 admin.site.register(BlogPost)
