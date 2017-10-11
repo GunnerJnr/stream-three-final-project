a5d891dd093efe7b2a7a355ab3a1f40affdd161f
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-11 13:21:19 +0100

    Created first django view to return the blog post list of published posts

--- gamers_blog/views.py
+++ gamers_blog/views.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import BlogPost
+
 
 # Create your views here.
+
+# define a view to return a list of our blog posts
+def blog_post_list(request):
+    blog_posts = BlogPost.published.all()
+    return render(request, 'gamers_blog/blog_post/blog_post_list.html',
+                           {'blog_posts', blog_posts})
