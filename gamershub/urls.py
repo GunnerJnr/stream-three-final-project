8adb4e9d984cf2b2b07989a07875c4002e356249
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-13 13:01:24 +0100

    updated the gamers hub main application urls.py to include the accounts app

--- gamershub/urls.py
+++ gamershub/urls.py
@@ -21,11 +21,14 @@ urlpatterns = [
     # add the url to access the admin panel
     url(r'^admin/', include(admin.site.urls)),
 
+    # accounts app urls
+    url(r'^accounts/', include('accounts.urls'))
+
     # home app urls
     url(r'^$', home_views.get_index, name='home'),
 
     # here we want to add the urls from gamersblog app, we also assign
     # a namespace so we can easily access this group of urls
-    url(r'^blog/', include('gamersblog.urls', namespace='gamersblog',
-                           app_name='gamersblog'))
+    url(r'^blog/', include('gamersblog.urls', namespace='gamersblog', app_name='gamersblog'))
+
 ]
