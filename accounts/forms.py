990c3414b034be0cc2b7c4cce6a3014c7b1b9ed3
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-13 12:21:31 +0100

    Created a forms.py file for the accounts app, this will be the basis of a usr login system, for now it uses the built in Django auth, this will probably over time be modified to use custom login, and maybe even add social login of some kind, perhaps down the line look into integrating Gravatar for profile pics ?

---
+++ accounts/forms.py
@@ -0,0 +1,16 @@
+"""
+    Froms.py:
+    This will handle user authenticationagainst our DB
+"""
+from django import forms
+
+
+class UserLoginForm(forms.Form):
+    """
+    username: The users name input field
+    password: The users password input field
+    We use the widget here to display the passwords HTML input, it includes a
+    type="password" attr
+    """
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
