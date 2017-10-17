4df03262a5b0b601611d223531a9f082a6b90328
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-17 17:01:07 +0100

    moved some images into better named directories for concise labelling, updated css to reflect thisand also any paths to imags throughout the html pages, also have added a view and a profile.html page, this redirects the user to their profile page upon successfully logging in to the site, also have added to settings.py the lazy_reverse django calls to redirect between login, logout..

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login, logout, logout_then_login
+from accounts import views as accounts_views
 
 urlpatterns = [
     # accounts app URLs
     # django login views
     url(r'^login/$', login, name='login'),
+    # accounts/profile views
+    url(r'^$', accounts_views.user_profile, name='user_profile'),
     # django logout views
     url(r'^logout/$', logout, name='logout'),
     url(r'^logout-then-login', logout_then_login, name='logout_then_login'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,10 @@
 # -  *  - coding:utf-8 -  *  -
 from __future__ import unicode_literals
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
+@login_required
+def user_profile(request):
+    return render(request, 'accounts/profile.html', {'section': 'profile'})
