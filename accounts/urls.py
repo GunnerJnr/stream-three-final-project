2ad1051a0049ea8e8adec26d984e47f03c5328e8
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-18 17:16:07 +0100

    Added 2 new html pages to handle the password_change and another to display a success message when it is done, also updated the logged in/out html pages and changed the div ids to classes, this makes more sense as an ID should be unique, because we are reusing it through multiple html files a class makes a lot more sense, also the urls.py has been updated to reflect the change_password states, I have also added a button to the profile page that if the user clicks will take them to the change password page as currently you can only navigate to this by typing the url manaully, perhaps later on the button might be replaced with a simple link, at present the profile page is very basic

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,21 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout, logout_then_login
+from django.contrib.auth import views as auth_views
 from accounts import views as accounts_views
 
+
+# accounts app URLs
 urlpatterns = [
-    # accounts app URLs
     # django login views
-    url(r'^login/$', login, name='login'),
+    url(r'^login/$', auth_views.login, name='login'),
+
     # accounts/profile views
     url(r'^$', accounts_views.user_profile, name='user_profile'),
+
     # django logout views
-    url(r'^logout/$', logout, name='logout'),
-    url(r'^logout-then-login', logout_then_login, name='logout_then_login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout-then-login', auth_views.logout_then_login, name='logout_then_login'),
+
+    # change password views
+    url(r'^password-change/$', auth_views.password_change, name='password_change'),
+    url(r'^password-change/done/$', auth_views.password_change_done, name='password_change_done'),
 ]
