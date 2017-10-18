a32af3e1d143c3d271f890634b09beffb59d8334
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-18 20:54:32 +0100

    added new url views to accounts urls.py, this will be for the password reset functionality, now need to create the forms to go with the urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,18 +9,27 @@ urlpatterns = [
     # https://docs.djangoproject.com/en/1.11/topics/auth/default/#module-django.contrib.auth.views
 
     # django login views
+    # https://docs.djangoproject.com/en/1.11/topics/auth/default/#how-to-log-a-user-in
     url(r'^login/$', auth_views.login, name='login'),
 
     # django logout views
+    # https://docs.djangoproject.com/en/1.11/topics/auth/default/#how-to-log-a-user-out
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^logout-then-login', auth_views.logout_then_login, name='logout_then_login'),
 
     # change password views
+    # https://docs.djangoproject.com/en/1.11/topics/auth/default/#changing-passwords
     url(r'^password-change/$', auth_views.password_change, name='password_change'),
     url(r'^password-change/done/$', auth_views.password_change_done, name='password_change_done'),
 
-    # custom user views
+    # https://docs.djangoproject.com/en/1.11/topics/auth/default/#django.contrib.auth.views.PasswordResetView
+    # password reset views
+    url(r'^password-reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password-reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^password-reset/confirm/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^password-reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
+    # custom user views
     # accounts/profile views
     url(r'^$', accounts_views.user_profile, name='user_profile'),
 ]
