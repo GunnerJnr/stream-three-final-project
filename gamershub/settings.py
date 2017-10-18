0970e1cb794aac28634eaae032df00ab28dcd0b4
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-18 13:32:06 +0100

    removed secret.py and stored the secret key using os.environ to load it instead, we set it in the console using the set GAMERSHUB_SECRET_KEY = 'some-string-value', also adding a link to base html for the admin panel, it checks if the user is an admin or superuser and if they are this link will bedisplayed

--- gamershub/settings.py
+++ gamershub/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 from django.core.urlresolvers import reverse_lazy
-import secret
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret.GAMERSHUB_SECRET_KEY
+SECRET_KEY = os.environ['GAMERSHUB_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
