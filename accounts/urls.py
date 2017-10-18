13b4a20090d457b1239e386bdfde9a2ea9b2628b
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-18 22:25:40 +0100

    big BUG in the urls.py I had forgotten to add the regex for generating the unique id in the email link for resetting the password, hopefully this will fix the issue

--- accounts/urls.py
+++ accounts/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     # password reset views
     url(r'^password-reset/$', auth_views.password_reset, name='password_reset'),
     url(r'^password-reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
-    url(r'^password-reset/confirm/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^password-reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
     # Please see the below for the password_reset_email.html
