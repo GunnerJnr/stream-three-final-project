0cd4b12e6368613d340a9d8f5b2e9dfd65b827c8
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-19 18:42:07 +0100

    added new class to handle registering a user, we ask for first and last name, a username and an email address, we also ask for a password and use some validation to check if both passwords match, if they do we want to register the new user

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,8 +3,38 @@
     This will handle user authenticationagainst our DB
 """
 from django import forms
+from django.contrib.auth.models import User
 
 
+# This class will be responsible for handling the registration of a new user
+class UserRegisterForm(forms.ModelForm):
+    """
+    password1: The users password to enter initially
+    password2: The users password re-entered
+    """
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+
+    class Meta:
+        """
+        model: The user model
+        fields: The fields we wish to provide the user with for registration/sign-up
+        """
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+    def clean_password(self):
+        """
+        clean_password: Here we check if the second password matches the first password
+        entered, if they do not match we return a validation error message to the user
+        """
+        cd = self.cleaned_data
+        if cd['password1'] != cd['password2']:
+            raise forms.ValidationError('Sorry, the passwords do not match!')
+        return cd['password2']
+
+
+# This class will be responsible for handling the user login
 class UserLoginForm(forms.Form):
     """
     username: The users name input field
