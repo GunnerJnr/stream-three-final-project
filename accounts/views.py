7553e26ef443c99e65b931e2393bb74c77605047
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-19 18:53:29 +0100

    created the register method, this basically checks if the user exists if they dont redirects them to the register page, if they do then asks them to fill out the fields, and if all the fields are valid it goes ahead, creates the user and then redirects to a success message and tells them they can now log in

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,29 @@
 # -  *  - coding:utf-8 -  *  -
 from __future__ import unicode_literals
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth import views
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from accounts.forms import UserRegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegisterForm(request.POST)
+        if user_form.is_valid():
+            # Create a new 'User' object
+            new_user = user_form.save(commit=False)
+            # Set the password to the users chosen password
+            new_user.set_password(user_form.cleaned_data['password1'])
+            # save the new User object
+            new_user.save()
+            # finally return the registered html page request
+            return render(request, 'accounts/register_done.html',  {'new_user': new_user})
+    else:
+        # if the user is not registered we wish to display the registration html page
+        user_form = UserRegisterForm()
+    return render(request, 'accounts/register.html',  {'user_form': user_form})
 
 
 @login_required
 def user_profile(request):
-    return render(request, 'accounts/profile.html', {'section': 'profile'})
+    return render(request, 'accounts/profile.html',  {'section': 'profile'})
