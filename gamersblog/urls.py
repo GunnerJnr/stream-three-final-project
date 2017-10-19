4f37d7af1298036c5719fa947072211ff2409ee1
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-19 17:29:32 +0100

    reordered url for the blog to be all on a single line for better readability

--- gamersblog/urls.py
+++ gamersblog/urls.py
@@ -7,7 +7,5 @@ urlpatterns = [
 
     # we use regex here, we say the day and month need 2 digits,
     # and the year needs 4 digits, also the post can be words or hyphens
-    url(r'^(?P<day>\d{2})/(?P<month>\d{2})/(?P<year>\d{4})/'\
-        r'(?P<post>[-\w]+)/$',
-        gamersblog_views.blog_post_detail, name='blog_post_detail'),
+    url(r'^(?P<day>\d{2})/(?P<month>\d{2})/(?P<year>\d{4})/' r'(?P<post>[-\w]+)/$', gamersblog_views.blog_post_detail, name='blog_post_detail'),
 ]
