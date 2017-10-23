ebcc1b3e4486e0bd2235bc47e52bba3bb6914fa9
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-23 19:41:08 +0100

    here we have added a view which allows the user to edit their profile (if a valid one exists) NOTE this should not be an issue for any new users signed up from this point as a profile will be created for the automatically, as stated previously any old users had to have one created manually in the admin panel. We also check if everything was in order then display a success message to the user, if the updated fields were invalid then we tell the user there was an error and dont let them save any changes until everything is correct

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 # from django.contrib.auth import views
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from accounts.forms import UserRegisterForm
+from django.contrib import messages
+from accounts.forms import UserRegisterForm, EditUserForm, EditProfileForm
+from accounts.models import Profile
 
 
 # this method handles user registration
@@ -17,6 +19,8 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password1'])
             # save the new User object
             new_user.save()
+            # create the users profile
+            profile = Profile.objects.create(user=new_user)
             # finally return the registered html page request
             return render(request, 'accounts/register_done.html',  {'new_user': new_user})
     else:
@@ -30,3 +34,20 @@ def register(request):
 # this method redirects the user to their profile page on successful login
 def user_profile(request):
     return render(request, 'accounts/profile.html',  {'section': 'profile'})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = EditUserForm(instance=request.user, data=request.POST)
+        profile_form = EditProfileForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Sorry, but there was an error updating your profile')
+    else:
+        user_form = EditUserForm(instance=request.user)
+        profile_form = EditProfileForm(instance=request.user.profile)
+    return render(request, 'accounts/edit_profile.html', {'user_form': user_form, 'profile_form': profile_form})
