38d289f42c4291e3bfb560f7099fbba86035cfea
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-23 19:42:24 +0100

    added a few of the user profile fields to the admin panel for ease of editing (non user)

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from accounts.models import Profile
+
 
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date_of_birth', 'profile_image']
+
+
+admin.site.register(Profile, ProfileAdmin)
