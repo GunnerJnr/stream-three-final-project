72098e03231af07d633c6a1bacfdb54ee8bc8d13
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-30 12:05:24 +0000

    updated views so that the post title has to be unique for the blog posts

--- gamersblog/models.py
+++ gamersblog/models.py
@@ -51,8 +51,8 @@ class BlogPost(models.Model):
 
     # Create our settings that will be used in our blog posts.
     post_author = models.ForeignKey(User, related_name='blog_posts')
-    post_title = models.CharField(max_length=40)
-    post_slug = models.SlugField(max_length=50, unique=True, unique_for_date='publish')
+    post_title = models.CharField(max_length=40, unique=True)
+    post_slug = models.SlugField(max_length=50, unique_for_date='publish')
     post_body = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
