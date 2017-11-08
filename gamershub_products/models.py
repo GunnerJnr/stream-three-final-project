1469907bd8efbc8179c01fe929ebce8248430467
GunnerJnr <gunnerjnr@live.co.uk>
2017-11-08 17:40:38 +0000

    Here we have created a model for basic products that the user can buy and creatd a basic HTML template, just note that the template uses the .sandbox instead of the .render for the test environment, better description to follow
    The end of that statement ends in .sandbox. This means that our button is set to send all transactions to the test side of PayPal. In a production environment, this should use the .render method to point all customers to the real PayPal handling systems, as in:
            <td>{{ product.paypal_form.render }}</td>

--- gamershub_products/models.py
+++ gamershub_products/models.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
+
 # Create your models here.
+class GamersHubProducts(models.Model):
+    length=255
+    image_width = 200
+    image_height = 200
+
+    product_images = models.ImageField(upload_to='gamershub/product_images/%d/%m/%Y', height_field='image_height', width_field='image_width', blank=True)
+    name = models.CharField(max_length=length, ${blank=True, null=True} default='')
+    description = models.TextField()
+    price = models.DecimalField(max digits=6, decimal places=2)
