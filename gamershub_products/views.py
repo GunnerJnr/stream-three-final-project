77401d246c541d6ab82ccbbe095a8469ab870a58
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-11-09 21:30:22 +0000

    had to make a major change to stop users accessing the store page without being logged in

--- gamershub_products/views.py
+++ gamershub_products/views.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import GamersHubProducts
 
 
 # Create your views here.
+@login_required
 def all_products(request):
     products = GamersHubProducts.objects.all()
     return render(request, "products/products.html", {"products": products})
