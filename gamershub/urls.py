f024572bb90d016ae2472151bb9e54a48227d55b
gunnerjnr <gunnerjnr@live.co.uk>
2017-11-21 19:08:34 +0000

    typo

--- gamershub/urls.py
+++ gamershub/urls.py
@@ -48,5 +48,5 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
 
     # Static Root urls
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
