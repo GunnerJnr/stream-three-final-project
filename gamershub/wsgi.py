e6e4a1d751e064ef3d4b7b64ee4ee88770b222c1
gunnerjnr <gunnerjnr@live.co.uk>
2017-11-21 18:45:14 +0000

    forgot to change the path for the staged settings in wsgi file

--- gamershub/wsgi.py
+++ gamershub/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamershub.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.staging")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
