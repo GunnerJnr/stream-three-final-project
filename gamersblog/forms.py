c9008b5b51c4dd9a5e26400406ae7225e5f2910e
gunnerjnr <gunnerjnr@live.co.uk>
2017-11-21 14:00:17 +0000

    removed the post.author from the blog post form as it is no longer needed

--- gamersblog/forms.py
+++ gamersblog/forms.py
@@ -5,4 +5,4 @@ from gamersblog.models import BlogPost
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
-        fields = ('post_title', 'post_body', 'post_images', 'post_author')
+        fields = ('post_title', 'post_body', 'post_images')
