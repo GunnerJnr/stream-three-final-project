a81291b60f589fec4a81743d6e7b1d9793e40f6f
gunnerjnr <gunnerjnr@live.co.uk>
2017-11-22 12:46:24 +0000

    force static root to use the 'static' folder

--- settings/base.py
+++ settings/base.py
@@ -114,11 +114,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, STATICFILES_DIRS)
 
 # Media Root - serve the media files uploaded by users
 
