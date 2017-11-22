2c384449fa58a8cd5cf77a744c1c2878bacfc64f
gunnerjnr <gunnerjnr@live.co.uk>
2017-11-22 12:04:09 +0000

    Re pushing some minor changes with static

--- settings/base.py
+++ settings/base.py
@@ -111,10 +111,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, "static"),
 )
 
 # Media Root - serve the media files uploaded by users
