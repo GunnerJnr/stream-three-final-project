dde5b199baf664aec01ec63667232a1977695690
gunnerjnr <gunnerjnr@live.co.uk>
2017-11-24 11:30:49 +0000

    minor

--- settings/base.py
+++ settings/base.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['GAMERSHUB_SECRET_KEY']
 
-ALLOWED_HOSTS = ['127.0.0.1', u'0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 # Site ID
 SITE_ID = 2
