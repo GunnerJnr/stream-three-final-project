f2b5ce463877f53118ecd8248537711f6f4616f2
gunnerjnr <gunnerjnr@live.co.uk>
2017-11-24 12:58:22 +0000

    trying hardcoded smtp values

--- settings/staging.py
+++ settings/staging.py
@@ -27,12 +27,13 @@ ALLOWED_HOSTS.append('gamershub.herokuapp.com')
 # https://docs.djangoproject.com/en/1.11/topics/email/#django.core.mail.backends.smtp.EmailBackend
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'GAMERSHUB_MAIL_HOST'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'GAMERSHUB_HOST_USER'
 EMAIL_HOST_PASSWORD = 'GAMERSHUB_EMAIL_PASSWORD'
 DEFAULT_FROM_EMAIL = 'GAMERSHUB_DEFAULT_FROM_EMAIL'
 EMAIL_PORT = 587  # default SMTP port 587
-EMAIL_USE_TLS = False  # Whether to use a TLS (secure) connection when talking to the SMTP server
+EMAIL_USE_TLS = True  # Whether to use a TLS (secure) connection when talking to the SMTP server
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Log DEBUG information to the console
 LOGGING = {
