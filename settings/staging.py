4093e9197dfebcf12eb0b64ffd31de23503ac55b
gunnerjnr <gunnerjnr@live.co.uk>
2017-11-24 12:22:51 +0000

    trying port as a numeric value as opposed to an env var it might be that heroku is changing it to a string opposed to an int

--- settings/staging.py
+++ settings/staging.py
@@ -31,7 +31,7 @@ EMAIL_HOST = 'GAMERSHUB_MAIL_HOST'
 EMAIL_HOST_USER = 'GAMERSHUB_HOST_USER'
 EMAIL_HOST_PASSWORD = 'GAMERSHUB_EMAIL_PASSWORD'
 DEFAULT_FROM_EMAIL = 'GAMERSHUB_DEFAULT_FROM_EMAIL'
-EMAIL_PORT = 'GAMERSHUB_EMAIL_PORT'  # default SMTP port 587
+EMAIL_PORT = 465  # default SMTP port 587
 EMAIL_USE_TLS = True  # Whether to use a TLS (secure) connection when talking to the SMTP server
 
 # Log DEBUG information to the console
