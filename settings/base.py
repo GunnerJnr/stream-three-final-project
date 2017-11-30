606f8c96c2f9ba641e1be19e622807772167deae
gunnerjnr <gunnerjnr@live.co.uk>
2017-11-30 10:47:05 +0000

    minor

--- settings/base.py
+++ settings/base.py
@@ -114,8 +114,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
@@ -123,5 +123,5 @@ STATICFILES_DIRS = (
 
 # Media Root - serve the media files uploaded by users
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
