7130ada138c07d44d8bb33389973f51bb3a92fb0
gunnerjnr <gunnerjnr@live.co.uk>
2017-11-30 16:00:50 +0000

    having to push changes manually for collect static, heroku problems

---
+++ staticfiles/js/theme-select.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+    var styleCSS = document.getElementById('theme-css');
+    var styleChoice = document.getElementById('theme');
+
+    if(!localStorage.getItem('theme')) {
+        populateStorage();
+    } else {
+        setStyles();
+    }
+
+    function populateStorage() {
+        localStorage.setItem('theme', document.getElementById('theme').value);
+
+        setStyles();
+    }
+
+    function setStyles() {
+        var currentChoice = localStorage.getItem('theme');
+
+        document.getElementById('theme').value = currentChoice;
+
+        styleCSS.setAttribute('href', currentChoice);
+    }
+
+    styleChoice.onchange = populateStorage;
+}
