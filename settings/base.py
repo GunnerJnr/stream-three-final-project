4c8f1f452daef4c841353657cd303d619db16ccf
gunnerjnr <gunnerjnr@live.co.uk>
2017-11-30 17:34:09 +0000

    more changes for amazon storage

--- settings/base.py
+++ settings/base.py
@@ -142,3 +142,5 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 # Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
 # you run `collectstatic`).
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
