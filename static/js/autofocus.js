0e717b2d6911af24bd0a6daa196831fc5da1edfc
gunnerjnr <gunnerjnr@live.co.uk>
2017-12-12 13:59:20 +0000

    removed window.onload and $(document).load due to a clash between autofocus and theme select, if one worked the other stopped, perhaps needs looking into further..

--- static/js/autofocus.js
+++ static/js/autofocus.js
@@ -1,5 +1,2 @@
-// once the window has loaded
-window.onload = function () {
-    // target the id of 'username' and auto focus into the input field.
-    $("#id_username").focus();
-};
+// target the id of 'username' and auto focus into the input field.
+$("#id_username").focus();
--- static/js/theme-select.js
+++ static/js/theme-select.js
@@ -1,40 +1,36 @@
-// When the window has loaded
-window.onload = function () {
+// create two variables that will store ID's
+var styleCSS = document.getElementById('theme-css');
+var styleChoice = document.getElementById('theme');
 
-    // create two variables that will store ID's
-    var styleCSS = document.getElementById('theme-css');
-    var styleChoice = document.getElementById('theme');
+// here we check to see if the local storage exists
+if (!localStorage.getItem('theme')) {
+    // if it doesn't then we populate it
+    populateStorage();
+} else {
+    // otherwise we set the already stored chosen style
+    setStyles();
+}
 
-    // here we check to see if the local storage exists
-    if(!localStorage.getItem('theme')) {
-        // if it doesn't then we populate it
-        populateStorage();
-    } else {
-        // otherwise we set the already stored chosen style
-        setStyles();
-    }
+// we define a function that will handle populating the storage
+function populateStorage() {
+    // we set the items value of which we wish to store in local storage
+    localStorage.setItem('theme', document.getElementById('theme').value);
 
-    // we define a function that will handle populating the storage
-    function populateStorage() {
-        // we set the items value of which we wish to store in local storage
-        localStorage.setItem('theme', document.getElementById('theme').value);
+    // now we call our setStyles function
+    setStyles();
+}
 
-        // now we call our setStyles function
-        setStyles();
-    }
+// create a function that will handle changing the style of the site to the chosen theme
+function setStyles() {
+    // fetch and store the current choice
+    var currentChoice = localStorage.getItem('theme');
 
-    // create a function that will handle changing the style of the site to the chosen theme
-    function setStyles() {
-        // fetch and store the current choice
-        var currentChoice = localStorage.getItem('theme');
+    // set the element to the current choice
+    document.getElementById('theme').value = currentChoice;
 
-        // set the element to the current choice
-        document.getElementById('theme').value = currentChoice;
+    // set the chosen attribute to the current choice
+    styleCSS.setAttribute('href', currentChoice);
+}
 
-        // set the chosen attribute to the current choice
-        styleCSS.setAttribute('href', currentChoice);
-    }
-
-    // call on our populate storage function
-    styleChoice.onchange = populateStorage;
-};
+// call on our populate storage function
+styleChoice.onchange = populateStorage;
