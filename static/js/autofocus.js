2eb0c242a0b6d15ecc9d596d9b721b619cf44bd6
gunnerjnr <gunnerjnr@live.co.uk>
2017-12-12 23:03:52 +0000

    added a delay and screen scroll by 150px so the javascript should scroll to and auto focus on the element username for login and register

--- static/js/autofocus.js
+++ static/js/autofocus.js
@@ -1,2 +1,10 @@
-// target the id of 'username' and auto focus into the input field.
-$("#id_username").focus();
+// Create a function that will handle delaying of auto focus to the input field
+function delayFocusInput() {
+    // scroll the window by 200 pixels vertically
+    window.scrollBy(0, 150);
+    // target the id of 'username' and auto focus into the input field.
+    $("#id_username").focus();
+};
+
+// use setTimeout() to execute
+setTimeout(delayFocusInput, 1000);
