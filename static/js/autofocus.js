a36889dc3266aff7e7bb3fa7a1f1d908aa8d7b1b
gunnerjnr <gunnerjnr@live.co.uk>
2017-12-13 11:17:56 +0000

    changed comment to match pixel value on the Y axis as it still stated 200 but we had changed to 150

--- static/js/autofocus.js
+++ static/js/autofocus.js
@@ -1,6 +1,6 @@
 // Create a function that will handle delaying of auto focus to the input field
 function delayFocusInput() {
-    // scroll the window by 200 pixels vertically
+    // scroll the window by 150 pixels vertically
     window.scrollBy(0, 150);
     // target the id of 'username' and auto focus into the input field.
     $("#id_username").focus();
